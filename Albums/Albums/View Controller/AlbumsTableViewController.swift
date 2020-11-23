0634a37de68855cfc5dc9cea0f7be2178c818579
Alfredo Colon <alfredoDevelops@gmail.com>
2020-11-22 23:14:51 -0800

    Followed along with class examples for this one as it is easily doable and I didn't want to spend too much time perfecting an answer

---
+++ Albums/Albums/View Controller/AlbumsTableViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  Albums
+//
+//  Created by Alfredo Colon on 11/22/20.
+//
+
+import UIKit
+
+class AlbumsTableViewController: UITableViewController {
+    
+    var albumController = AlbumController()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        albumController.getAlbums(completion: { (error) in
+            if let error = error {
+                print(error)
+                return
+            }
+            
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+            
+        })
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return albumController.albums.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AlbumCell", for: indexPath)
+
+        let album = albumController.albums[indexPath.row]
+        cell.textLabel?.text = album.name
+        cell.detailTextLabel?.text = album.artist
+
+        return cell
+    }
+    
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if let albumDetailVC = segue.destination as? AlbumDetailTableViewController {
+            albumDetailVC.albumController = albumController
+            
+            if segue.identifier == "ShowAlbumDetailSegue", let indexPath = tableView.indexPathForSelectedRow {
+                albumDetailVC.album = albumController.albums[indexPath.row]
+            }
+        }
+        
+        
+    }
+
+}
---
+++ Albums/Albums/Views/SongTableViewCell.swift
@@ -0,0 +1,64 @@
+//
+//  SongTableViewCell.swift
+//  Albums
+//
+//  Created by Alfredo Colon on 11/22/20.
+//
+
+import UIKit
+
+protocol SongTableViewCellDelegate {
+    func addsong(with title: String, duration: String)
+}
+
+class SongTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var songTitleTextField: UITextField!
+    @IBOutlet weak var songDurationTextField: UITextField!
+    @IBOutlet weak var addSongButton: UIButton!
+    
+    var delegate: SongTableViewCellDelegate?
+    
+    var song: Song? {
+        didSet {
+            updateViews()
+        }
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func updateViews() {
+        if let song = song {
+            songTitleTextField.text = song.name
+            songDurationTextField.text = song.duration
+            addSongButton.isHidden = true
+        } else {
+            addSongButton.isHidden = false
+        }
+    }
+    
+    override func prepareForReuse() {
+        songTitleTextField.text = ""
+        songDurationTextField.text = ""
+        addSongButton.isHidden = false
+    }
+    
+    @IBAction func addSongButtonTapped(_ sender: Any) {
+        guard let title = songTitleTextField.text,
+            let duration = songDurationTextField.text,
+            !title.isEmpty,
+            !duration.isEmpty else { return }
+        delegate?.addsong(with: title, duration: duration)
+    }
+    
+
+}
